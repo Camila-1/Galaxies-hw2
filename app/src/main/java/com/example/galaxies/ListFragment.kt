ca39b839799573eb50ad3a89908a7c84c1120651
Camila <camilabalova@gmail.com>
2019-10-31 05:36:58 +0200

    layout_land added

--- app/src/main/java/com/example/galaxies/ListFragment.kt
+++ app/src/main/java/com/example/galaxies/ListFragment.kt
@@ -1,15 +1,18 @@
 package com.example.galaxies
 
 import android.os.Bundle
+import android.os.Parcel
+import android.os.Parcelable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.parcel.Parcelize
 import kotlinx.android.synthetic.main.fragment_list.*
 
-data class Item(val image: Int, val title: String, val description: String, val details: String = "details")
+@Parcelize
+data class Item(val image: Int, val title: String, val description: String, val details: String = "details") : Parcelable
 
 class ListFragment : Fragment() {
 
@@ -26,15 +29,15 @@ class ListFragment : Fragment() {
 
 
     private fun initItems() = listOf(
-        Item(R.drawable.milky_way, getString(R.string.milky_way_title), getString(R.string.milky_way_description)),
-        Item(R.drawable.messier_61, getString(R.string.messier_61_title), getString(R.string.messier_61_description)),
-        Item(R.drawable.m51, getString(R.string.m51_title), getString(R.string.m51_description)),
-        Item(R.drawable.hoags_object, getString(R.string.hoags_object_title), getString(R.string.hoags_object_description)),
-        Item(R.drawable.ngc_1300, getString(R.string.ngc_1300_title), getString(R.string.ngc_1300_description)),
-        Item(R.drawable.ngc_1672, getString(R.string.ngc_1672_title), getString(R.string.ngc_1672_description)),
-        Item(R.drawable.ngc_247, getString(R.string.ngc_247_title), getString(R.string.ngc_247_description)),
-        Item(R.drawable.ngc_5584, getString(R.string.ngc_5584_title), getString(R.string.ngc_5584_description)),
-        Item(R.drawable.ngc_6240, getString(R.string.ngc_6240_title), getString(R.string.ngc_6240_description)),
-        Item(R.drawable.ngc_6872, getString(R.string.ngc_6872_title), getString(R.string.ngc_6872_description))
+        Item(R.drawable.milky_way, getString(R.string.milky_way_title), getString(R.string.milky_way_description), getString(R.string.milky_way_details)),
+        Item(R.drawable.messier_61, getString(R.string.messier_61_title), getString(R.string.messier_61_description), getString(R.string.messier_61_details)),
+        Item(R.drawable.m51, getString(R.string.m51_title), getString(R.string.m51_description), getString(R.string.m51_details)),
+        Item(R.drawable.hoags_object, getString(R.string.hoags_object_title), getString(R.string.hoags_object_description), getString(R.string.hoags_object_details)),
+        Item(R.drawable.ngc_1300, getString(R.string.ngc_1300_title), getString(R.string.ngc_1300_description), getString(R.string.ngc_1300_details)),
+        Item(R.drawable.ngc_1672, getString(R.string.ngc_1672_title), getString(R.string.ngc_1672_description), getString(R.string.ngc_1672_details)),
+        Item(R.drawable.ngc_247, getString(R.string.ngc_247_title), getString(R.string.ngc_247_description), getString(R.string.ngc_247_details)),
+        Item(R.drawable.ngc_5584, getString(R.string.ngc_5584_title), getString(R.string.ngc_5584_description), getString(R.string.ngc_5584_details)),
+        Item(R.drawable.ngc_6240, getString(R.string.ngc_6240_title), getString(R.string.ngc_6240_description), getString(R.string.ngc_6240_details)),
+        Item(R.drawable.ngc_6872, getString(R.string.ngc_6872_title), getString(R.string.ngc_6872_description), getString(R.string.ngc_6872_details))
     )
 }
--- app/src/main/java/com/example/galaxies/MainActivity.kt
+++ app/src/main/java/com/example/galaxies/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.galaxies
 
+import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.widget.Toast
 import androidx.core.os.bundleOf
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,21 +12,46 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 //    private val listFragment = ListFragment()
 //    private val detailsFragment = DetailsFragment()
+    private var checkedItem: Item? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val orientation: Int = resources.configuration.orientation
         val fragmentManager = supportFragmentManager
         val transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.list_fragment, ListFragment()).commit()
+        if(orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            transaction.replace(R.id.list_fragment, ListFragment()).replace(R.id.details_fragment, DetailsFragment()).commit()
+        } else if(orientation == Configuration.ORIENTATION_PORTRAIT){
+            transaction.replace(R.id.fragment, ListFragment()).commit()
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle, outPersistentState: PersistableBundle) {
+        super.onSaveInstanceState(outState, outPersistentState)
+        outState.putParcelable("item", checkedItem)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        checkedItem = savedInstanceState.getParcelable("item")
     }
 
     fun itemClicked(item: Item) {
+        checkedItem = item
         val fragmentManager = supportFragmentManager
         val transaction = fragmentManager.beginTransaction()
-        transaction.replace(R.id.list_fragment, DetailsFragment.newInstance(item.details))
+        transaction.replace(R.id.fragment, DetailsFragment.newInstance(item.details))
             .addToBackStack(null)
             .commit()
     }
+
+//    private fun renderPortrait() {
+//        val fragmentManager = supportFragmentManager
+//        val transaction = fragmentManager.beginTransaction()
+//        transaction.replace(R.id.list_fragment, DetailsFragment.newInstance(item.details))
+//            .addToBackStack(null)
+//            .commit()
+//    }
 }
